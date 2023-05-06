1b439b35ba0cf7bd63e72d0283778bd6fbcd1d1c
skybluelee <kusdk@naver.com>
2023-05-06 23:08:48 +0900

    crawling.v2

--- comments_pipeline/crawling_functions.py
+++ comments_pipeline/crawling_functions.py
@@ -8,6 +8,30 @@ import time
 import os
 import urllib.request
 from selenium.webdriver.common.keys import Keys
+from datetime import datetime
+
+# 출판사, 제목, 기자, 본문, 댓글창
+def main(driver):
+    time.sleep(1)
+    total = driver.find_element(By.CLASS_NAME, "end_container")
+    title_area = total.find_element(By.CLASS_NAME, "newsct_wrapper._GRID_TEMPLATE_COLUMN._STICKY_CONTENT")
+    title_info = title_area.find_element(By.CLASS_NAME, "media_end_head_title")
+    title = title_info.text
+    pub_info = title_area.find_element(By.CLASS_NAME, "media_end_head_top")
+    pub = pub_info.find_element(By.CSS_SELECTOR, "img").get_attribute("title")
+    rep_info = title_area.find_element(By.CLASS_NAME, "media_end_head_journalist")
+    reporter = rep_info.text
+    article_info = title_area.find_element(By.ID, "dic_area")
+    article = article_info.text
+
+    try: # 호감순 댓글 확인 가능
+        more_comments = title_area.find_element(By.CLASS_NAME, "u_cbox_btn_view_comment")
+    except: # 댓글 확인 불가
+        more_comments = title_area.find_element(By.CLASS_NAME, "simplecmt_link.is_navercomment")
+    more_comments.click()
+
+    time.sleep(0.5)
+    return pub, title, reporter, article
 
 # 더보기 클릭
 def more_comments(driver):
@@ -32,7 +56,53 @@ def comments_analysis(driver):
     total = comments[0].find_elements(By.CSS_SELECTOR, "span")[0].text
     self_removed = comments[1].find_elements(By.CSS_SELECTOR, "span")[0].text
     auto_removed = comments[2].find_elements(By.CSS_SELECTOR, "span")[0].text
-    return total, self_removed, auto_removed
+
+    male, female, age_10, age_20, age_30, age_40, age_50, age_60 = '', '', '', '', '', '', '', ''
+    try:
+        comments_two = comments_num.find_element(By.CLASS_NAME, "u_cbox_chart_wrap.u_cbox_chart_open")
+        comments_sex_age = comments_two.find_element(By.CLASS_NAME, "u_cbox_chart_cont_inner")
+        comments_sex = comments_sex_age.find_element(By.CLASS_NAME, "u_cbox_chart_sex")
+        comments_male = comments_sex.find_element(By.CLASS_NAME, "u_cbox_chart_progress.u_cbox_chart_male")
+        male = comments_male.find_elements(By.CSS_SELECTOR, "span")[0].text[:-1]
+        comments_female = comments_sex.find_element(By.CLASS_NAME, "u_cbox_chart_progress.u_cbox_chart_female")
+        female = comments_female.find_elements(By.CSS_SELECTOR, "span")[0].text[:-1]
+
+        comments_age = comments_sex_age.find_element(By.CLASS_NAME, "u_cbox_chart_age")
+        comments_age_total = comments_age.find_elements(By.CLASS_NAME, "u_cbox_chart_progress")
+        age_list = []
+        for i in comments_age_total:
+            age_list.append(i.text[:-5])
+        age_10, age_20, age_30, age_40, age_50, age_60 = age_list[0], age_list[1], age_list[2], age_list[3], age_list[4], age_list[5][:-1]
+    except:
+        pass
+    now = datetime.now()
+
+    return total, self_removed, auto_removed, male, female, age_10, age_20, age_30, age_40, age_50, age_60, now
+
+# 전체 댓글 수집
+def comments(driver):
+    # time.sleep(1)
+    comment_area = driver.find_element(By.CLASS_NAME, "newsct_wrapper._GRID_TEMPLATE_COLUMN._STICKY_CONTENT")
+    total_comments = comment_area.find_element(By.ID, "cbox_module")
+    total_comment = total_comments.find_element(By.ID, "cbox_module_wai_u_cbox_content_wrap_tabpanel")
+    comments = total_comment.find_elements(By.CSS_SELECTOR, "li")
+    comments_list = []
+    for i in comments:
+        temp = []
+        temp.append(i.find_element(By.CLASS_NAME, "u_cbox_text_wrap").text)
+        try:
+            good_bad = i.find_element(By.CLASS_NAME, "u_cbox_recomm_set")
+            good_bads = good_bad.find_elements(By.CSS_SELECTOR, "a > em")
+            for j in good_bads:
+                temp.append(j.text)
+        except:
+            pass
+        if len(temp) == 1:
+            temp.append('')
+            temp.append('')
+        comments_list.append(temp)
+
+    return comments_list
 
 # 이 상품의 태그
 def hash_tag(driver):
--- comments_pipeline/crawling_test.py
+++ comments_pipeline/crawling_test.py
@@ -6,17 +6,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import time
 
-from crawling_functions import more_comments, comments_analysis
+from crawling_functions import more_comments, comments_analysis, main, comments
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://n.news.naver.com/article/comment/119/0002709755")
+# driver.get("https://n.news.naver.com/article/comment/119/0002709755")
+# driver.get("https://n.news.naver.com/article/119/0002709755")
+driver.get("https://n.news.naver.com/mnews/article/005/0001606450")
+# driver.get("https://n.news.naver.com/mnews/article/comment/005/0001606450")
 
-# while(1):
-#     try:
-#         more_comments(driver)
-#     except:
-#         break
-# time.sleep(2)
+# a = comments_analysis(driver)
+# print(a)
 
-a,b,c = comments_analysis(driver)
-print(a,b,c)
+pub, title, reporter, article = main(driver) # 본문에서 크롤링 후 댓글창으로 이동
+total, self_removed, auto_removed, male, female, age_10, age_20, age_30, age_40, age_50, age_60, now = comments_analysis(driver)
+while(1): # 모든 댓글이 나올때까지 더보기 클릭
+    try:
+        more_comments(driver)
+    except:
+        break
+comments_list = comments(driver)
+print(len(comments_list))
