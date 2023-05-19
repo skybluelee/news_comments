9a0daa07fbf563afae5cbfa9ee05952fc1f4df12
skybluelee <kusdk@naver.com>
2023-05-19 14:33:52 +0900

    s3

--- airflow/dags/mysql_to_s3.py
+++ airflow/dags/mysql_to_s3.py
@@ -13,6 +13,7 @@ import psycopg2
 import json
 from airflow.sensors.external_task import ExternalTaskSensor
 from airflow.providers.mysql.hooks.mysql import MySqlHook
+import pandas as pd
 
 def get_MySQL_connection():
     hook = MySqlHook(mysql_conn_id='mysql')
@@ -20,16 +21,26 @@ def get_MySQL_connection():
     cur = conn.cursor()
     return conn, cur
 
+def get_title():
+    conn, cur = get_MySQL_connection()
+    sql = "SELECT title FROM comments_db.articles;"
+    df = pd.read_sql_query(sql,conn)
+    return df['title'][0], df['title'][1]
+
 def delete_table():
     conn, cur = get_MySQL_connection()
-    sql = 'DELETE FROM comments_db.users_dist;'
+    sql = 'DELETE FROM comments_db.user_distribution_1;'
     cur.execute(sql)
-    sql = 'DELETE FROM comments_db.comments;'
+    sql = 'DELETE FROM comments_db.comments_1;'
+    cur.execute(sql)
+    sql = 'DELETE FROM comments_db.user_distribution_2;'
+    cur.execute(sql)
+    sql = 'DELETE FROM comments_db.comments_2;'
     cur.execute(sql)
     conn.commit() 
 
 dag = DAG(
-    dag_id = 'mysql_s3_test',
+    dag_id = 'mysql_to_s3',
     start_date = datetime(2022,8,24), # 날짜가 미래인 경우 실행이 안됨
     schedule = '0 8 * * *',  # etl이 끝나는 시간에 맞춤
     max_active_runs = 1,
@@ -40,32 +51,20 @@ dag = DAG(
     }
 )
 
-# waiting_for_etl = ExternalTaskSensor(
-#     task_id='waiting_for_etl',
-#     external_dag_id='Comment_Extract',
-#     external_task_id='etl',
-#     timeout=10*60,
-#     # execution_date_fn=lambda x: x,
-#     mode='reschedule',
-#     dag = dag
-# )
-
 etl = PythonOperator(
     task_id = 'delete_table',
     python_callable = delete_table,
     dag = dag
 )
 
-schema = "kusdk"
-table = "nps"
 s3_bucket = "news-comments"
-s3_key = schema + "-" + table       # s3_key = schema + "/" + table
+title_1, title_2 = get_title()
 
-mysql_to_s3_user_dist = SqlToS3Operator(
-    task_id = 'mysql_to_s3',
+mysql_to_s3_user_distribution_1 = SqlToS3Operator(
+    task_id = 'mysql_to_s3_user_distribution_1',
     query = "SELECT * FROM comments_db.comments",
     s3_bucket = s3_bucket,
-    s3_key = "user_distribution",
+    s3_key = "user_distribution" + title_1,
     sql_conn_id = "mysql",
     aws_conn_id = "aws",
     verify = False,
@@ -74,11 +73,11 @@ mysql_to_s3_user_dist = SqlToS3Operator(
     dag = dag
 )
 
-mysql_to_s3_comments = SqlToS3Operator(
-    task_id = 'mysql_to_s3_nps',
+mysql_to_s3_comments_1 = SqlToS3Operator(
+    task_id = 'mysql_to_s3_comments_1',
     query = "SELECT * FROM comments_db.comments",
     s3_bucket = s3_bucket,
-    s3_key = "comments",
+    s3_key = "comments" + title_1,
     sql_conn_id = "mysql",
     aws_conn_id = "aws",
     verify = False,
@@ -87,4 +86,30 @@ mysql_to_s3_comments = SqlToS3Operator(
     dag = dag
 )
 
-mysql_to_s3_user_dist >> mysql_to_s3_comments>> etl
+mysql_to_s3_user_distribution_2 = SqlToS3Operator(
+    task_id = 'mysql_to_s3_user_distribution_2',
+    query = "SELECT * FROM comments_db.comments",
+    s3_bucket = s3_bucket,
+    s3_key = "user_distribution" + title_2,
+    sql_conn_id = "mysql",
+    aws_conn_id = "aws",
+    verify = False,
+    replace = True,
+    pd_kwargs={"index": False, "header": False},    
+    dag = dag
+)
+
+mysql_to_s3_comments_2 = SqlToS3Operator(
+    task_id = 'mysql_to_s3_comments_2',
+    query = "SELECT * FROM comments_db.comments",
+    s3_bucket = s3_bucket,
+    s3_key = "comments" + title_2,
+    sql_conn_id = "mysql",
+    aws_conn_id = "aws",
+    verify = False,
+    replace = True,
+    pd_kwargs={"index": False, "header": False},    
+    dag = dag
+)
+
+mysql_to_s3_user_distribution_1 >> mysql_to_s3_comments_1 >> mysql_to_s3_user_distribution_2 >> mysql_to_s3_comments_2 >> etl
